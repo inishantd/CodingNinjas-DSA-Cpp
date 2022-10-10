b2eaa0397b488fb6bac8f58ac4d3f65d6c6457b4
Nishant Deshmukh <110892133+inishantd@users.noreply.github.com>
2022-10-10 18:48:57 +0530

    Create Last Index of Number.cpp

---
+++ Recursion 1/Last Index of Number/Last Index of Number.cpp
@@ -0,0 +1,22 @@
+int lastIndex(int input[], int size, int x) {
+  /* Don't write main().
+     Don't read input, it is passed as function argument.
+     Return output and don't print it.
+     Taking input and printing output is handled automatically.
+  */
+    if(size == 0){
+			return -1;
+    }
+    int smallanswer = lastIndex(input + 1, size - 1, x);
+    if(smallanswer != -1){
+			return smallanswer + 1;
+    }
+    if(input[0] == x){
+			return 0;
+    }
+    else
+    {
+		return -1;
+    }
+
+}
