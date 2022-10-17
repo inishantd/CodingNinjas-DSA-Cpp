45145e4779e1f970d8aa2b21672e8c070cdc76b8
Nishant Deshmukh <110892133+inishantd@users.noreply.github.com>
2022-10-18 02:03:35 +0530

    Create Length of LL.cpp

---
+++ Linked List 1/Length of LL/Length of LL.cpp
@@ -0,0 +1,32 @@
+/****************************************************************
+ 
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+int length(Node *head)
+{
+    //Write your code here
+       if(head == NULL){
+       return 0;
+   }
+    int total = 0;
+    while (head != NULL){
+ 	total++;
+        head = head->next;
+        }
+    return total;
+
+}
