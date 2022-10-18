444fc3e9b58ac5c15810ac7056667251ea01a8d3
Nishant Deshmukh <110892133+inishantd@users.noreply.github.com>
2022-10-18 11:00:27 +0530

    Create Length of LL(recursive).cpp

---
+++ Linked List 1/Length of LL(recursive)/Length of LL(recursive).cpp
@@ -0,0 +1,23 @@
+/****************************************************************
+ 
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+int length(Node *head) {
+	if(head == NULL)
+        return 0;
+    else 
+        return 1+length(head->next);
+}
