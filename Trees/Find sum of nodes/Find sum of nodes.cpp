270b875fa1813f0100df085486951ba9b3baff1f
Nishant Deshmukh <110892133+inishantd@users.noreply.github.com>
2022-10-26 17:48:06 +0530

    Create Find sum of nodes.cpp

---
+++ Trees/Find sum of nodes/Find sum of nodes.cpp
@@ -0,0 +1,11 @@
+int sumOfNodes(TreeNode<int>* root) {
+    // Write your code here
+    if(root == NULL){
+        return 0;
+    }
+    int ans = root->data;
+    for(int i=0; i<root->children.size(); i++){
+        ans += sumOfNodes(root->children[i]);
+    }
+    return ans;
+}
