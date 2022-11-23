0fe4b98deb5d25329156e5490ccf903b03f7ba2e
Nishant Deshmukh <110892133+inishantd@users.noreply.github.com>
2022-11-23 23:54:52 +0530

    Create Mirror.cpp

---
+++ Binary Trees/Mirror/Mirror.cpp
@@ -0,0 +1,28 @@
+/********************
+	Following is the Binary Tree Node class structure
+
+	template <typename T>
+	class BinaryTreeNode {
+    	public : 
+    	T data;
+    	BinaryTreeNode<T> *left;
+    	BinaryTreeNode<T> *right;
+
+    	BinaryTreeNode(T data) {
+        	this -> data = data;
+        	left = NULL;
+        	right = NULL;
+    	}
+	};
+
+*********************/
+
+void mirrorBinaryTree(BinaryTreeNode<int>* root) {
+    // Write your code here
+    if(root == NULL){
+        return ;
+    }
+    mirrorBinaryTree(root->left);
+    mirrorBinaryTree(root->right);
+    swap(root->left , root-> right);
+}
